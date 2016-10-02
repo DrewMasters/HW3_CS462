25a18f180f61b93245fe509e0df1d1ebf9290a44
Drew Masters <drewmasters78@gmail.com>
2016-10-02 15:09:57 -0400

    updated matrix multiply code so that it uses a array of arrays of doubles

--- matrix_multiply_tester.c
+++ matrix_multiply_tester.c
@@ -3,36 +3,52 @@
 
 int main(int argc, char **argv){
 	int i,j,k,n;
-	double *A,*B,*C,sum;
+	double **A,**B,**C,sum,*tmpA,*tmpB;
 	FILE *f;
 	
 	n=atoi(argv[1]);
-	A=(double *)malloc(sizeof(double)*n*n);
-	B=(double *)malloc(sizeof(double)*n*n);
-	C=(double *)malloc(sizeof(double)*n*n);
+	tmpA=(double *)malloc(sizeof(double)*n*n);
+	tmpB=(double *)malloc(sizeof(double)*n*n);
+
+	A=(double **)malloc(sizeof(double *)*n);
+	B=(double **)malloc(sizeof(double *)*n);
+	C=(double **)malloc(sizeof(double *)*n);
+	
+	for(i=0;i<n;i++){
+		A[i]=(double *)malloc(sizeof(double)*n);
+		B[i]=(double *)malloc(sizeof(double)*n);
+		C[i]=(double *)malloc(sizeof(double)*n);
+	}
 
 	f = fopen("A.dat","rb");
-	fread(A,sizeof(double),n*n,f);
+	fread(tmpA,sizeof(double),n*n,f);
 	fclose(f);
 
 	f = fopen("B.dat","rb");
-	fread(B,sizeof(double),n*n,f);
+	fread(tmpB,sizeof(double),n*n,f);
 	fclose(f);
 	
+	for (i=0; i<n; i++){
+		for (j=0; j<n; j++){
+			A[i][j]=tmpA[i*j];
+			B[i][j]=tmpB[i*j];
+		}
+	}
+
 	sum=0;
 	for (i=0;i<n;i++){
 		for(j=0;j<n;j++){
 			for(k=0;k<n;k++){
-				sum = sum + A[i*k]*B[k*j];
+				sum = sum + A[i][k]*B[k][j];
 			}
-			C[i*j]=sum;
+			C[i][j]=sum;
 			sum=0;
 		}
 	}
 
 	for (i=0;i<n;i++){
 		for (j=0;j<n;j++){
-			printf("%f ",C[i*j]);
+			printf("%f ",C[i][j]);
 		}
 		printf("\n");
 	}
