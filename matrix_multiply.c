85a4d8f993b29cc938eb216331cb7b05d1c5ccb8
Drew Masters <drewmasters78@gmail.com>
2016-10-03 19:02:07 -0400

    shares a single element from matrix B with a single process and a entire colum from matrix A and calculates the product of each column entry from A and the single column entry from A

--- matrix_multiply.c
+++ matrix_multiply.c
@@ -4,9 +4,9 @@
 #include <math.h>
 
 int main(int argc, char **argv){
-	int i,j,world_size, world_rank,n;
+	int i,j,row,col,t,world_size, world_rank,n;
 	FILE *f;
-	double *A, *B, *C,tmp;
+	double *A, *B, *C,*dot,*tc,tmp,va,vb;
 	/*
 	 * Initialize MPI
 	 */
@@ -28,6 +28,8 @@ int main(int argc, char **argv){
 		MPI_Finalize();
 		exit(1);
 	}
+	row = world_rank % n;
+	col = world_rank / n;
 	/*
 	 * Create matrix objects
 	 */
@@ -35,6 +37,8 @@ int main(int argc, char **argv){
 	A = (double *)malloc(sizeof(double)*n*n);
 	B = (double *)malloc(sizeof(double)*n*n);
 	C = (double *)malloc(sizeof(double)*n*n);
+	dot = (double *)malloc(sizeof(double)*n);
+	tc = (double *)malloc(sizeof(double)*n);
 	//printf("%d matrices are initialized\n",world_rank);
 	
 	/*
@@ -52,30 +56,60 @@ int main(int argc, char **argv){
 		fclose(f);
 		
 		printf("%d finished reading in matrices\n",world_rank);
-	
+		va = A[0];
+		vb = B[0];
+		for (i=0; i < n; i++) tc[i]=B[i];
 		for (i=1;i<n*n;i++){
-			for (j=0; j<n*n; j++)
+			/*for (j=0; j<n*n; j++)
 			{
 				MPI_Send(&A[j],1,MPI_DOUBLE,i,0,MPI_COMM_WORLD);
 				MPI_Send(&B[j],1,MPI_DOUBLE,i,1,MPI_COMM_WORLD);
 			}
+			*/
+			/*
+			 * send each process it's matrix element
+			 */
+			MPI_Send(&A[i],1,MPI_DOUBLE,i,0,MPI_COMM_WORLD);
+			MPI_Send(&B[i],1,MPI_DOUBLE,i,1,MPI_COMM_WORLD);
+			printf("col: %d sending to %d\n",i/n,i);
+			MPI_Send(&A[n*(i/n)],n,MPI_DOUBLE,i,2,MPI_COMM_WORLD);
 		}
 	}
 	else{
-		for (j=0; j<n*n; j++){
+		printf("%d about to start receive matrix\n",world_rank);
+		/*for (j=0; j<n*n; j++){
 			MPI_Recv(&A[j],1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 			MPI_Recv(&B[j],1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		}
+		*/
+		/*
+		 * receive element from root process
+		 */
+		MPI_Recv(&va,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(&vb,1,MPI_DOUBLE,0,1,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(&tc[0],n,MPI_DOUBLE,0,2,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 	}
+
+	printf("%d va: %f row: %d col: %d\n",world_rank,va,row,col);
+	printf("%d vb: %f row: %d col: %d\n",world_rank,vb,row,col);
+	for (i=0;i<n;i++) printf("%d tc[%d]: %f\n", world_rank, i, tc[i]);
+	/*
+	 * take the first row of A and multiply it
+	 * by the value received for B
+	 * all elements in first column of A is 
+	 * multiplied by value in B
+	 */
+	for (i=0;i<n;i++) dot[i]=vb*tc[i];
+
 	/*
 	 * Distribute matrix across all processes
 	 */
 	//MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	printf("%d about to print matrix\n",world_rank);
+	/*printf("%d about to print matrix\n",world_rank);
 	for (i=0;i<n*n;i++){
 		printf("%d A i:%d %f\n",world_rank,i,A[i]);
 		printf("%d B i:%d %f\n",world_rank,i,B[i]);
-	}
+	}*/
 	/*
 	 * Multiply matrix
 	 */
