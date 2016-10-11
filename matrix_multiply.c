310c15d8c9b4e5b261788637513a8a651db6dd5e
Drew Masters <drewmasters78@gmail.com>
2016-10-10 21:36:37 -0400

    dot product and gather code finished. Just need to clean up code and comment

--- matrix_multiply.c
+++ matrix_multiply.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv){
 	int i, j, root, row, col, t, r_rank, r_size,
 		c_rank, c_size, world_size, world_rank, n, k;
 	FILE *f;
-	double *A, *B, *C,*tr, *tc, va, vb;
+	double *A, *B, *C,*tr, *tc, va, vb, dot;
 	MPI_Comm r, c;
 	
 	/*
@@ -82,7 +82,6 @@ int main(int argc, char **argv){
 		k=0;
 		for (i=0; i < n; i++){
 			for (j=i; j < world_size; j+=n){
-				printf("1 i: %d j: %d k: %d\n",i,j,k);
 				if (j==0){
 					va = A[j];
 					vb = B[j];
@@ -102,65 +101,80 @@ int main(int argc, char **argv){
 		MPI_Recv(&va, 1, MPI_DOUBLE, root, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		MPI_Recv(&vb, 1, MPI_DOUBLE, root, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	}
-	printf("row: %d col: %d world_rank: %d va: %f vb: %f\n",
-			row, col, world_rank, va, vb);
+	//printf("row: %d col: %d world_rank: %d va: %f vb: %f\n",
+	//		row, col, world_rank, va, vb);
 
 	/*
-	 * calculate dot product
+	 * construct rows and columns for dot product
 	 */
 	MPI_Comm_rank(r, &r_rank);
 	MPI_Comm_rank(c, &c_rank);
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	//printf("%d about to create row array\n",world_rank);
 	MPI_Allgather(&va, 1, MPI_DOUBLE, tr, 1, MPI_DOUBLE, r);
 
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_rank(r, &r_rank);
 	MPI_Comm_rank(c, &c_rank);
-	//printf("%d about to create column array\n",world_rank);
 	MPI_Allgather(&vb, 1, MPI_DOUBLE, tc, 1, MPI_DOUBLE, c);
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	MPI_Comm_rank(r, &r_rank);
 	MPI_Comm_rank(c, &c_rank);
-	//printf("%d finish both gathers\n",world_rank);
 
-	MPI_Comm_rank(r, &r_rank);
-	MPI_Comm_rank(c, &c_rank);
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	/*
-	printf("%d about to print row array\n",world_rank);
+	/*printf("%d about to print row array\n",world_rank);
 	for (i=0; i < n; i++){
 		printf("%d tr[%d]: %f\n",world_rank, i, tr[i]);
 	}
-	*/
+	
 	MPI_Comm_rank(r, &r_rank);
 	MPI_Comm_rank(c, &c_rank);
 
-	/*
+	
 	printf("%d about to print column array\n",world_rank);
 	for (i=0; i < n; i++){
 		printf("%d tc[%d]: %f\n",world_rank, i, tc[i]);
 	}
 	*/
+	/*
+	 * calculate dot product
+	 */
+	dot = 0;
+	for (i=0; i<n; i++){
+		dot = dot + tr[i]*tc[i];
+	}
+
 	if (world_rank==0){
 		/*
 		 * receive elements from other processes
 		 * and place elements in correct position
 		 * of matrix/array C
 		 */
-
+		k=0;
+		for (i=0; i < n; i++){
+			for (j=i; j < world_size; j+=n){
+				if (j==0){
+					C[k]=dot;
+				}
+				else{
+					MPI_Recv(&C[k], 1, MPI_DOUBLE, j, 0, MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+				}
+				k++;
+			}
+		}
 
 		/*
 		 * write C to C.dat
 		 */
+		for (i=0; i < world_size; i++){
+			printf("%d C[%d]: %f\n",world_rank,i,C[i]);
+		}
 
 	}
 	else{
 		/*
 		 * send result back to root
 		 */
-
+		MPI_Send(&dot, 1, MPI_DOUBLE, root, 0, MPI_COMM_WORLD);
 	}
 	MPI_Finalize();
 }
